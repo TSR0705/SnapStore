e6993ad986c041f9a15d87d92a559a030cae1663
TANMAY SINGH <tanmaysingh8246@gmail.com>
2026-05-07 11:13:34 +0530

    Add MonitoringException for monitoring engine errors

---
+++ src/main/java/com/filex/engine/MonitoringException.java
@@ -0,0 +1,15 @@
+package com.filex.engine;
+
+/**
+ * Exception thrown when monitoring engine operations fail.
+ */
+public class MonitoringException extends Exception {
+
+    public MonitoringException(String message) {
+        super(message);
+    }
+
+    public MonitoringException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
